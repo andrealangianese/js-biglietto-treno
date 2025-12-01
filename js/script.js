4f5b528fb5c2e35c2646084a209db044ba7c686f
andrealangianese <andre.langia.01@gmail.com>
2025-12-01 14:04:59 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//INPUT
+    // chiedo a utente quanti kilometri vuole percorrere
+const 
+
+    // chiedo a utente l'età del passeggero per eventuali scontistiche
+// ELABORAZIONE 
+    //prezzo biglietto = 0,21 €/ km
+    //sconto del 20% se ha meno di 18 anni
+    //sconto del 40% se ha più di 65 anni
+//OUTPUT stampare con log e poi eventualmente da elaborare in pagina
