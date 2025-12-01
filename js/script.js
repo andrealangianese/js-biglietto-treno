4729d6a405f84fb7bb3acd1c487a047c75601cc6
andrealangianese <andre.langia.01@gmail.com>
2025-12-01 14:16:41 +0100

    feat: correzione link js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 //INPUT
     // chiedo a utente quanti kilometri vuole percorrere
-const 
+const distanceToDo = parseInt(prompt("Scegli quanti kilometri vuoi percorrere!!"));
 
     // chiedo a utente l'età del passeggero per eventuali scontistiche
+
+const userAge = parseInt(prompt("Scrivi la tua età per eventuali sconti"));
 // ELABORAZIONE 
     //prezzo biglietto = 0,21 €/ km
     //sconto del 20% se ha meno di 18 anni
