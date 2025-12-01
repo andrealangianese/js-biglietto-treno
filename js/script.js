2cb6579d80f6271067d49e1b92301f4119588be6
andrealangianese <andre.langia.01@gmail.com>
2025-12-01 15:15:23 +0100

    feat: risultato ottenuto

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
 //INPUT
     // chiedo a utente quanti kilometri vuole percorrere
-const distanceToDo = parseInt(prompt("Scegli quanti kilometri vuoi percorrere!!"));
+let distanceToDo = parseInt(prompt("Scegli quanti kilometri vuoi percorrere!!"));
+console.log(distanceToDo);
 
     // chiedo a utente l'età del passeggero per eventuali scontistiche
 
-const userAge = parseInt(prompt("Scrivi la tua età per eventuali sconti"));
+let userAge = parseInt(prompt("Scrivi la tua età per eventuali sconti"));
+console.log(userAge);
+
+const kmPrice = 0.21;
 // ELABORAZIONE 
     //prezzo biglietto = 0,21 €/ km
+
+let ticketPrice = kmPrice * distanceToDo ;
+console.log(ticketPrice);
+
     //sconto del 20% se ha meno di 18 anni
+
+if (userAge < 18){
+    discountPrice = (ticketPrice / 100) * 80;
+} 
+else if (userAge > 65) {
+   discountPrice = ( ticketPrice / 100) * 60;
+}
+console.log (discountPrice)
     //sconto del 40% se ha più di 65 anni
 //OUTPUT stampare con log e poi eventualmente da elaborare in pagina
